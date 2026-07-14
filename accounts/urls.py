ce0965e053ac89667956d2f76c478d922f2b1f4e
vikasgarg2105 <vikasgarg2105@gmail.com>
2026-07-14 22:42:39 +0530

    login

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.login_view, name="login"),
     path("signup/", views.signup, name="signup"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
     path("forgot-password/", views.forgot_password, name="forgot_password")
 ]
